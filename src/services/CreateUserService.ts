9a5e2a13f215204401d0fcffb3773a72497bb164
Jean Paulo <jeanpauloalves@gmail.com>
2020-12-07 21:40:06 -0300

    first commit

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,34 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    });
+    if (checkUserExists) {
+      throw new Error('Email address alreadry used.');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = await usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return usersRepository.save(user);
+  }
+}
+
+export default CreateUserService;
