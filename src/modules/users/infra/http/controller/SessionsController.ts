a9fee67b702baa69937e5245ce15c8e1f22b978d
jeanp13 <jeanpauloalves@gmail.com>
2021-06-08 21:31:16 -0300

    template email provider

--- src/modules/users/infra/http/controller/SessionsController.ts
+++ src/modules/users/infra/http/controller/SessionsController.ts
@@ -5,7 +5,7 @@ import { container } from 'tsyringe';
 export default class SessionController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
-    console.log(request.body);
+    // console.log(request.body);
     const authenticateUser = container.resolve(AuthenticateUserService);
 
     const { user, token } = await authenticateUser.execute({
--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,10 +1,17 @@
 import nodemailer, { Transporter } from 'nodemailer';
+import { inject, injectable } from 'tsyringe';
+import IMailTemplateProvider from '../../MailTemplateProvider/models/IMailTemplateProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
 import IMailProvider from '../models/IMailProvider';
 
+@injectable()
 export default class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
 
-  constructor() {
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider,
+  ) {
     // console.log('asd');
     nodemailer.createTestAccount().then(account => {
       const tranporter = nodemailer.createTransport({
@@ -22,12 +29,23 @@ export default class EtherealMailProvider implements IMailProvider {
     });
   }
 
-  public async sendMail(to: string, body: string): Promise<void> {
+  public async sendMail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
     const info = await this.client.sendMail({
-      from: 'Equipe goBarber <equipe@gobarber.com.br',
-      to,
-      subject: 'Recuperação de senha',
-      text: body,
+      from: {
+        name: from?.name || 'Equipe GoBarber',
+        address: from?.email || 'equipe@gobarber.com.br',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await this.mailTemplateProvider.parse(templateData),
     });
     console.log('Message sent: %s', info.messageId);
     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
